89a22d52fc0778016776a3c0abd30d705b52eb3c
Jeffrey <jefftorres021@gmail.com>
2024-08-23 10:44:07 -0500

    Cross done?

--- scripts/balance.py
+++ scripts/balance.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Datos de ejemplo
 DEMANDA = 40
 capacidades = np.array([4, 8, 20, 20, 12])
 
@@ -7,88 +8,67 @@ X = np.array([[0, 0, 20, 20, 0], [0, 8, 15, 5, 12]])
 N = X.shape[1]
 
 
-def alter_lower(single, idx_mut, capacidades):
-    delta = capacidades[idx_mut]
-    ca = list(np.nonzero(single)[0])
-    idx = -1
+# Mutar capacidad de un individuo
+def mutar_cap(individuo):
+    idx_mut = np.random.randint(individuo.shape)
+    # False = Menor
+    # True = Mayor
+    diff = False
 
-    single[idx_mut] = delta
+    if individuo[idx_mut] == 0:
+        delta = capacidades[idx_mut]
+        individuo[idx_mut] = capacidades[idx_mut]
+        diff = True
 
-    while np.sum(single) != DEMANDA:
-        if len(ca) > 0:
-            idx = np.random.choice(ca)
-            ca.remove(idx)
+    else:
+        delta = individuo[idx_mut]
+        individuo[idx_mut] = 0
 
-        if single[idx] - delta >= 0:
-            single[idx] -= delta
-        else:
-            delta -= single[idx]
-            single[idx] = 0
-
-
-def alter_upper(single, idx_mut, capacidades):
-    delta = single[idx_mut]
-    ca = list(np.where(single == 0)[0])
-    idx = -1
-
-    single[idx_mut] = 0
-
-    while np.sum(single) != DEMANDA:
-        if len(ca) > 0:
-            idx = np.random.choice(ca)
-            ca.remove(idx)
-
-        if capacidades[idx] == single[idx]:
-            continue
-
-        if single[idx] + delta > capacidades[idx]:
-            delta -= capacidades[idx] - single[idx]
-            single[idx] = capacidades[idx]
-        else:
-            single[idx] += delta
-            delta = 0
+    return igualar(individuo, delta, diff)
 
 
-def mutar_cap(individuo, idx_mut):
-    delta = capacidades[idx_mut]
-    individuo[idx_mut] = delta
+# Función para alterar el vector para que cumpla con la restricción de igualdad
+def igualar(individuo, delta, diff):
+    # Reducir para igualar a la demanda
+    if diff:
+        acopios = list(np.nonzero(individuo)[0])
 
-    return igualar(individuo)
+        while delta > 0 and len(acopios) > 0:
+            idx = np.random.choice(acopios)
+            acopios.remove(idx)
+
+            if delta <= individuo[idx]:
+                individuo[idx] -= delta[0]
+                delta = 0
+            else:
+                delta -= individuo[idx]
+                individuo[idx] = 0
+    # Aumentar para igualar a la demanda
+    else:
+        acopios = list(np.where(individuo == 0)[0])
+
+        while delta > 0 and len(acopios) > 0:
+            idx = np.random.choice(acopios)
+            acopios.remove(idx)
+
+            if delta <= capacidades[idx]:
+                individuo[idx] = delta[0]
+                delta = 0
+            else:
+                individuo[idx] = capacidades[idx]
+                delta -= capacidades[idx]
 
 
-def igualar(individuo):
-    ca = list(np.nonzero(individuo)[0])
-
-    while np.sum(individuo) != DEMANDA and len(ca) > 0:
-        idx = np.random.choice(ca)
-        ca.remove(idx)
-
-        if capacidades[idx] == individuo[idx]:
-            continue
-
-        # if np.sum(individuo) < DEMANDA:
-        #
-        # elif np.sum(individuo) > DEMANDA:
+def imprimir(individuo):
+    print(f'Vector: {individuo}, Suma: {np.sum(individuo)}')
 
 
-
-# def mutar_cero(individuo, idx_mut, capacidades):
-
-
-print(f'Iniciales')
-print(X[0])
-print(X[1])
+print(f'Estado inicial')
+print(f'Vector: {X[0]}, Suma: {np.sum(X[0])}')
+print(f'Vector: {X[1]}, Suma: {np.sum(X[1])}')
 
 for _ in range(1000):
-    print('EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
+    print('Mutados')
     for i in range(len(X)):
-        dx = np.random.randint(X.shape[1])
-
-        if X[i][dx] == 0:
-            alter_lower(X[i], dx, capacidades)
-        else:
-            alter_upper(X[i], dx, capacidades)
-        # if X[i][idx] != 0:
-        #     mutar_cap(X[i], idx)
-
-        print(f'Vector: {X[i]}, Suma: {np.sum(X[i])}')
+        mutar_cap(X[i])
+        imprimir(X[i])
