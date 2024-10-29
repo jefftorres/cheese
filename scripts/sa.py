108ca5af7ff5bb30a0fd2c465d4c843ef124c3f3
Jeffrey <jefftorres021@gmail.com>
2024-10-29 17:26:57 -0500

    Updated scripts/sa.py before model changes

--- scripts/sa.py
+++ scripts/sa.py
@@ -1,14 +1,25 @@
 import numpy as np
 
 
-def sa(t_max=1, t_min=1e-8, e_th=1e-5, alpha=0.91):
+def sa(t_max=1, t_min=1e-9, e_th=1e-5, alpha=0.84):
     np.random.seed(1)
-    t = t_max
+    x, count = _anneal(t_max, t_min, e_th, alpha)
+
+
+def f(x):
+    return x[0]**2 + x[1]**2
+
+
+def gen():
+    return np.random.uniform(-1, size=2)
+
+
+def _anneal(t, t_min, e_th, alpha):
     x = gen()
     e = f(x)
+    n = 0
 
     while t > t_min and e > e_th:
-        print(f'f(x): {e}\t\t\tx: {x}')
         dx = gen()
         de = f(dx)
 
@@ -26,14 +37,9 @@ def sa(t_max=1, t_min=1e-8, e_th=1e-5, alpha=0.91):
                 e = de
 
         t *= alpha
+        n += 1
 
-
-def f(x):
-    return x[0]**2 + x[1]**2
-
-
-def gen():
-    return np.random.uniform(-1, size=2)
+    return x, n
 
 
 if __name__ == '__main__':
